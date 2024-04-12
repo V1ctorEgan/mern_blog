a6f5b50f3c749a561fbc322322740145b25e0110
V1ctorEgan <victorifeanyi2004@gmail.com>
2024-04-12 22:20:58 +0100

    mongo db addition

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8000;
+const  {MongoClient} = require('mongodb')
 
 const articleInfo = {
     'learn-about-valves':{
@@ -20,6 +21,20 @@ const articleInfo = {
 app.use(express.json({extended: false}));
 
 app.get("/", (req, res)=> res.send('<h1> Thank you Jesus </h1>'))
+app.get("/api/articles/:name", async (req, res)=>{
+    try {
+        const articleName = req.params.name;
+        const client = await MongoClient.connect('mongodb://localhost:27017')
+        const db = client.db("dr")
+        const articleInfo =  await db.collection('articles').findOne({name: articleName});
+        res.status(200).json(articleInfo)
+        client.close();
+    } catch (error) {
+        res.status(500).json({message: 'Error connecting to database', error})
+    }
+   
+
+})
 
 app.post("/api/articles/:name/add-comments", (req, res)=>{
     const {username, text} = req.body
